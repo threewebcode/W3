de6afe2c298cba3865e3877ef06e03d22d6d23db
W3BUIlD <127061691+threewebcode@users.noreply.github.com>
2023-03-23 11:19:06 +0800

    Update ssh-key.sh

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-ssh-keygen -C threewebcode
-cat ~/.ssh/id_rsa.pub
+function key(){
+    ssh-keygen -C threewebcode
+    cat ~/.ssh/id_rsa.pub
+}
 
-git config --local user.name threewebcode
-git config --local user.email magestore@outlook.com
+function ssh(){
+    cp ssh/* ~/.ssh/
+}
+
+function git(){
+  echo $1
+  cd $1
+  git config --local user.name threewebcode
+  git config --local user.email magestore@outlook.com
+}
+
+$@
