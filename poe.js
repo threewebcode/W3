2e4b6ad670226e5152c49bbe6178e6e82b2b2e18
W3BUILD <127061691+threewebcode@users.noreply.github.com>
2023-06-09 10:00:03 +0800

    Update poe.js

--- poe.js
+++ poe.js
@@ -2,7 +2,7 @@ var aside = document.querySelector("aside");
 aside.remove();
 var nodes = document.getElementsByClassName("PageWithSidebarLayout_mainSection__i1yOg");
 for (var i = 0; i < nodes.length; i++) {
-  nodes[i].style.maxWidth = "auto";;
+  nodes[i].style.maxWidth = "1248px";
 };
 
 var nodes = document.getElementsByClassName("Message_botMessageBubble__CPGMI");
