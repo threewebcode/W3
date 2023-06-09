a87fe58f999bdb82d1cf520bce242bf4906953c1
W3BUILD <127061691+threewebcode@users.noreply.github.com>
2023-06-09 09:47:03 +0800

    Create poe.js

---
+++ poe.js
@@ -0,0 +1,12 @@
+var aside = document.querySelector("aside");
+aside.remove();
+var nodes = document.getElementsByClassName("PageWithSidebarLayout_mainSection__i1yOg");
+for (var i = 0; i < nodes.length; i++) {
+  nodes[i].style.maxWidth = "auto";;
+};
+
+var nodes = document.getElementsByClassName("Message_botMessageBubble__CPGMI");
+for (var i = 0; i < nodes.length; i++) {
+  node.classList.remove("Message_botMessageBubble__CPGMI");
+};
+
