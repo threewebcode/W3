3b4b44273e51b631dd9b6e98d0e83ad1919826af
threewebcode <phpjavaphp@gmail.com>
2025-03-09 00:00:56 +0800

    chore: make updates

--- script/poe.js
+++ script/poe.js
@@ -1,17 +1,2 @@
-var aside = document.querySelector("aside");
-aside.remove();
-var nodes = document.getElementsByClassName("PageWithSidebarLayout_mainSection__i1yOg");
-for (var i = 0; i < nodes.length; i++) {
-  nodes[i].style.maxWidth = "1248px";
-};
-
-var nodes = document.getElementsByClassName("Message_botMessageBubble__CPGMI");
-for (var i = 0; i < nodes.length; i++) {
-  nodes[i].style.maxWidth = "1248px";
-// node.classList.remove("Message_botMessageBubble__CPGMI");
-};
-
-var nodes = document.getElementsByClassName("markdown-body");
-for (var i = 0; i < nodes.length; i++) {
-  nodes[i].style.fontSize = "15px";
-};
+document.querySelector('.GrowingTextArea_textArea__ZWQbP').blur();
+document.querySelector('.GrowingTextArea_textArea__ZWQbP').focus();
