d559538b2faa9b991f22f7fcecbe6d2604bdd191
Afanti <phpjavaphp@gmail.com>
2025-03-13 11:16:32 +0800

    chore: make updates

--- script/copilot.js
+++ script/copilot.js
@@ -1,4 +1,7 @@
 document.querySelector("textarea#copilot-chat-textarea").focus();
-let container = document.querySelector(".message-container").parentElement;
-container.setAttribute('tabindex', '0');
-container.focus();
+function message_focus() {
+  let container = Array.from(document.querySelectorAll('div[class^="ImmersiveChat-module__messageContent"] .message-container')).pop();
+  container.setAttribute('tabindex', '0');
+  container.focus();
+}
+
