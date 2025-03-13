df93e00cb5bd0b99a5cdf930f9a79be0a35d2193
Afanti <phpjavaphp@gmail.com>
2025-03-13 10:44:30 +0800

    chore: make updates

--- script/copilot.js
+++ script/copilot.js
@@ -1,3 +1,4 @@
 document.querySelector("textarea#copilot-chat-textarea").focus();
-let container = document.querySelector(".message-container").parentElement.setAttribute('tabindex', '0');
+let container = document.querySelector(".message-container").parentElement;
+container.setAttribute('tabindex', '0');
 container.focus();
